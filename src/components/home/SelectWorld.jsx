4127465aeabfe4053f109fe310cf89655f0c632c
Ji-Sung05 <dlwltjd6847@gmail.com>
2024-10-04 12:30:17 +0900

    Feat: "여행 검색 필터링 적용"

--- src/components/home/SelectWorld.jsx
+++ src/components/home/SelectWorld.jsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useState } from 'react';
 import WorldItem from '../items/WorldItem';
 import { IoIosSearch } from "react-icons/io";
 import { Link } from 'react-router-dom';
@@ -38,11 +38,18 @@ const mockData = [
 
 // forwardRef를 사용하여 ref 전달 가능하도록 설정
 const SelectWorld = forwardRef((props, ref) => {
+  const [searchTerm, setSearchTerm] = useState(''); // 입력값을 저장하는 상태 추가
+
+  // 입력값에 따라 데이터를 필터링
+  const filteredData = mockData.filter((data) =>
+    data.title.toLowerCase().includes(searchTerm.toLowerCase()) || // 제목에서 검색
+    data.desc.toLowerCase().includes(searchTerm.toLowerCase())    // 설명에서 검색
+  );
   return (
     <div ref={ref} id={props.id} className='selectworld'>
       <div className='selectworld__input'>
         <IoIosSearch />
-        <input type="text" placeholder='어디로 여행을 떠나시나요?'/>
+        <input type="text" placeholder='어디로 여행을 떠나시나요?' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
       </div>
       <div className='selectworld__search'>
         <Link to={'/'}>
@@ -51,7 +58,7 @@ const SelectWorld = forwardRef((props, ref) => {
       </div>
       <div className='selectworld__items'>
         <ul>
-          {mockData.map((data) => (
+          {filteredData.map((data) => (
             <WorldItem data={data} key={data.id} />
           ))}
         </ul>
