f20e10316e465559030770babaa190700a5d0b80
Ji-Sung05 <dlwltjd6847@gmail.com>
2024-10-04 09:22:38 +0900

    Feat: "여행지 선택 아이템 구현"

--- src/components/items/WorldItem.jsx
+++ src/components/items/WorldItem.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import { MdLocationOn } from "react-icons/md";
+import { useContext } from 'react';
+import { ModalContext } from '../../pages/Home';
 
 const WorldItem = ({ data }) => {
+  const modal = useContext(ModalContext);
+
+  const handleClick = () => {
+    setTimeout(() => {
+      modal.handleOpen(data); // 상태 업데이트를 비동기로 처리
+    }, 0);
+  };
   return (
-    <li className='worldItem' key={data.id}>
+    <li className='worldItem' key={data.id} onClick={handleClick}>
       <MdLocationOn size={20} color='grey'/>
       <div>
         <p>{data.title}</p>
