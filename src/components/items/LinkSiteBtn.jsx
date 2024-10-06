0d6186f7faaae7ba0573281318f90517558b8862
Ji-Sung05 <dlwltjd6847@gmail.com>
2024-10-06 21:17:56 +0900

    Feat: "항공권, 숙소 버튼 구현"

--- src/components/items/LinkSiteBtn.jsx
+++ src/components/items/LinkSiteBtn.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const LinkSiteBtn = (props) => {
   return (
-    <Link id='linkSiteBtn' to={props.url}>
+    <Link id='linkSiteBtn' className={props.css} to={props.url}>
       <span>{props.icon}</span>
       <p>{props.site}</p>
     </Link>
