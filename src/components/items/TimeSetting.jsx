d888fb4ab9f95de0b1e49fec12a67f47939fa1a6
Ji-Sung05 <dlwltjd6847@gmail.com>
2024-10-12 00:51:07 +0900

    Feat: "날짜 처리 로직 적용"

--- src/components/items/TimeSetting.jsx
+++ src/components/items/TimeSetting.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 
-const TimeSetting = () => {
+const TimeSetting = ({ data }) => {
   return (
     <div id='timeSetting'>
       <div className='timeSetting__inner'>
         <div>
           <span>일자</span>
-          <span>10/15 화</span>
+          <span>{data.date}</span>
         </div>
         <div>
           <span>시작시간</span>
-          <input type="time" id="appt" name="appt" min="10:00" max="22:00" required />
+          <input type="time" id="appt" name="appt" value={data.startTime} required />
         </div>
         <div>
           <span>종료시간</span>
-          <input type="time" id="appt" name="appt" min="10:00" max="22:00" required />
+          <input type="time" id="appt" name="appt" value={data.endTime} required />
         </div>
       </div>
     </div>
