142cb5f77ba4df81048e096c056da5f55553cd86
Ji-Sung05 <dlwltjd6847@gmail.com>
2024-10-12 00:51:32 +0900

    Feat: "마커 제거"

--- src/pages/Planning.jsx
+++ src/pages/Planning.jsx
@@ -2,15 +2,23 @@ import React, { useState } from 'react';
 import Main from '../components/layout/Main';
 import DateRange from '../components/items/DateRange';
 import Modal from '@mui/material/Modal';
-import { Map, MapMarker } from 'react-kakao-maps-sdk';
+import { Map } from 'react-kakao-maps-sdk';
 
 const Planning = () => {
   const [open, setOpen] = useState(false);
   const handleClose = () => setOpen(false);
+
+  const mapStyles = {
+    width: 'calc(100% - 386px)',
+    height: '100vh',
+    marginLeft: '470px'
+  }
   return (
     <Main>
       <Modal
         open={open}
+        aria-labelledby="date-range-modal"
+        aria-describedby="select-date-range"
       >
         <div>
           <DateRange />
@@ -18,10 +26,9 @@ const Planning = () => {
       </Modal>
       <Map
         center={{ lat: 33.5563, lng: 126.79581 }}
-        style={{ width: 'calc(100% - 386px)', height: '100vh', marginLeft: '470px' }}
+        style={mapStyles}
         level={9}
       >
-        <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}></MapMarker>
       </Map>
     </Main>
   )
