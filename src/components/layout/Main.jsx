8a84815b09721f60d6cac90d45785b2e6c539ebf
Ji-Sung05 <dlwltjd6847@gmail.com>
2024-10-13 16:18:36 +0900

    Feat: "Main 페이지 context api 설정"

--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useState, createContext } from 'react'
 import { useParams } from 'react-router-dom'
 import Header from './Header'
 import Aside from './Aside';
 
+export const DateContext = createContext();
+
 const Main = ({ children }) => {
+  const [open, setOpen] = useState(true);
+
+  const [mockData, setMockData] = useState({
+    id: 1,
+    location: '제주',
+    startDate: "2024-10-08",
+    endDate: "2024-10-11",
+  });
+
   const param = useParams();
   return (
-    <div>
+    <DateContext.Provider value={{ mockData, setMockData, open, setOpen }}>
       {param.id ? <Aside /> : <Header />}
       <main id='main' className={param.id ? 'main' : ''}>
         {children}
       </main>
-    </div>
+    </DateContext.Provider>
   )
 }
 
