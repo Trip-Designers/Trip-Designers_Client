d44efa5bbe722960792a8965f6d8b0922329b259
Ji-Sung05 <dlwltjd6847@gmail.com>
2024-10-19 13:51:30 +0900

    Feat: "selectWorld 컴포넌트 수정" Style: "selectWorld css 수정"

--- src/components/home/SelectWorld.jsx
+++ src/components/home/SelectWorld.jsx
@@ -1,47 +1,19 @@
 import React, { forwardRef, useState } from 'react';
-import WorldItem from '../items/WorldItem';
-import { IoIosSearch } from "react-icons/io";
 import { Link } from 'react-router-dom';
+//컴포넌트
+import WorldItem from '../items/WorldItem';
+//아이콘
+import { IoIosSearch } from "react-icons/io";
+//데이터
+import { locationData } from '../../mockData';
 
-const mockData = [
-  {
-    id: 1,
-    title: '제주',
-    title_en: "JEJU",
-    desc: '대한민국',
-  },
-  {
-    id: 2,
-    title_en: "JEJU",
-    title: '하와이',
-    desc: '미국',
-  },
-  {
-    id: 3,
-    title_en: "JEJU",
-    title: '괌',
-    desc: '미국',
-  },
-  {
-    id: 4,
-    title_en: "JEJU",
-    title: '괌',
-    desc: '미국',
-  },
-  {
-    id: 5,
-    title_en: "JEJU",
-    title: '괌',
-    desc: '미국',
-  },
-]
 
 // forwardRef를 사용하여 ref 전달 가능하도록 설정
 const SelectWorld = forwardRef((props, ref) => {
   const [searchTerm, setSearchTerm] = useState(''); // 입력값을 저장하는 상태 추가
 
   // 입력값에 따라 데이터를 필터링
-  const filteredData = mockData.filter((data) =>
+  const filteredData = locationData.filter((data) =>
     data.title.toLowerCase().includes(searchTerm.toLowerCase()) || // 제목에서 검색
     data.desc.toLowerCase().includes(searchTerm.toLowerCase())    // 설명에서 검색
   );
