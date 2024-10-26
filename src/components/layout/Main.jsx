9e7d12c58594df02e29e826986b52ace64afe9f6
Ji-Sung05 <dlwltjd6847@gmail.com>
2024-10-26 11:25:38 +0900

    Feat: "Main DateContext 제거"

--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -1,31 +1,21 @@
-import React, { useState, createContext } from 'react'
+import React from 'react'
 import Header from './Header'
 import Aside from './Aside';
 import { useLocation } from 'react-router-dom';
 
-export const DateContext = createContext();
-
 const Main = ({ children }) => {
-  const [open, setOpen] = useState(true);
-
-  const [mockData, setMockData] = useState({
-    id: 1,
-    location: '',
-    startDate: "",
-    endDate: "",
-    theme: ""
-  });
 
   const location = useLocation();
-  console.log(location.pathname)
   
   return (
-    <DateContext.Provider value={{ mockData, setMockData, open, setOpen }}>
+    <>
       {location.pathname === '/' ? <Header /> : <Aside />}
-      <main className={location.pathname !== '/' ? 'nomain' : 'main'}>
+      <main 
+        className={location.pathname !== '/' ? 'nomain' : 'main'}
+      >
         {children}
       </main>
-    </DateContext.Provider>
+    </>
   )
 }
 
