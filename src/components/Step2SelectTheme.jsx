03879febbcd749d1b982371edd6c6fa37df5c136
Ji-Sung05 <dlwltjd6847@gmail.com>
2024-10-26 15:31:12 +0900

    Feat: "Stel2SelectTheme 데이터 작업"

--- src/components/Step2SelectTheme.jsx
+++ src/components/Step2SelectTheme.jsx
@@ -1,36 +1,39 @@
-import React, { useState, useContext } from 'react'
-import { DateContext } from './layout/Main';
-import { theme } from '../mockData';
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import { setTheme } from '../app/travleSlice';
+// 컴포넌트
 import Button from './items/Button';
 import Modal from '@mui/material/Modal';
 import ThemeModal from './items/ThemeModal';
+import { theme } from '../mockData';
 
 const Step2SelectTheme = () => {
-  const { mockData, setMockData } = useContext(DateContext);
-
+  const dispatch = useDispatch();
+  const travel = useSelector((state) => state.travel);
+  const [selectedTheme, setSelectedTheme] = useState(travel.theme);
+  
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
-  const handleThemeClick = (selectedTheme) => {
-    setMockData((prevData) => ({
-      ...prevData,
-      theme: selectedTheme,  // 클릭한 테마를 mockData.theme에 설정
-    }));
+  const handleThemeClick = (themeName) => {
+    setSelectedTheme(themeName);
+    dispatch(setTheme(themeName));  // Redux에 선택된 테마 저장
   };
+
   return (
     <div id='step2'>
       <div className='step2__inner'>
-        <h1>{mockData.location}</h1>
+        <h1>{travel.location}</h1>
         <div className='step2__info'>
-          <span>{mockData.startDate}</span> - <span>{mockData.endDate}</span>
+          <span>{travel.startDate}</span> - <span>{travel.endDate}</span>
         </div>
         <div className='step2__theme'>
           <h3>테마 선택하기</h3>
           <div className='step2__theme__item'>
             <ul>
               {theme.map((item, i) => (
-                <li key={i} onClick={() => handleThemeClick(item.theme)}>
+                <li key={i} onClick={() => handleThemeClick(item.theme)} className={selectedTheme === item.theme ? 'selected' : ''}>
                   <div><img src={item.img} alt="icon" /></div>
                   {item.theme}
                 </li>
