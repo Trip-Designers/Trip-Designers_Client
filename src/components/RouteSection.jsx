bd260d440f985e446975074afd55b3a102800bd0
Ji-Sung05 <dlwltjd6847@gmail.com>
2024-10-27 23:07:33 +0900

    Feat: "RouteSection 컴포넌트 생성"

--- src/components/RouteSection.jsx
+++ src/components/RouteSection.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Route from './items/Route'
 
-const RouteSection = () => {
+const RouteSection = ({ schedule, location, index }) => {
   return (
-    <div>RouteSection</div>
+    <div id='step1'>
+      <div className='step__top top'>
+        <h1>{location}</h1>
+        <div className='step__info'>
+          <span style={{fontSize: '1.1rem', fontWeight: 'bold'}}>{index}일차</span>
+        </div>
+      </div>
+      <div>
+        {schedule.activities.map((data, i) => (
+          <Route key={i} data={data} index={i+1} />
+        ))}
+      </div>
+    </div>
   )
 }
 
