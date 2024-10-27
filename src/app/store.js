54552a6fade9b9d5c96113eba6ae650f365ca265
Ji-Sung05 <dlwltjd6847@gmail.com>
2024-10-27 23:03:58 +0900

    Feat: "store에 day 리듀서 추가"

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from './apiSlice';
 import travleReducer from './travleSlice';
+import dayReducer from './daySlice';
 
 const store = configureStore({
   reducer: {
     travel: travleReducer,
+    day: dayReducer,
     [apiSlice.reducerPath]: apiSlice.reducer
   },
   middleware: (getDefaultMiddleware) =>
