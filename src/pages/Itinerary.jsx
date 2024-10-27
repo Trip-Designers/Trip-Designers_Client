eb82e00ebe0cc6c2f08da2fd6af6e15121dc8195
Ji-Sung05 <dlwltjd6847@gmail.com>
2024-10-27 23:05:36 +0900

    Feat: "itinerary 코드 수정"

--- src/pages/Itinerary.jsx
+++ src/pages/Itinerary.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Main from '../components/layout/Main';
 import { Map } from 'react-kakao-maps-sdk';
+import { useSelector } from 'react-redux';
 
 const Itinerary = () => {
-  const [open, setOpen] = useState(true);
-  const closeModal = () => {
-    setOpen(!open);
-  }
+  const data = useSelector((state) => state.travel.data);
+  const schedule = data.data.schedule || [];
+
+  const day = useSelector((state) => state.day.day);
+  const markers = schedule[day-1]?.activities || [];
   const mapStyles = {
     width: 'calc(100vw - 498px)',
     height: '100vh',
@@ -17,7 +19,7 @@ const Itinerary = () => {
       <Map
         center={{ lat: 33.5563, lng: 126.79581 }}
         style={mapStyles}
-        level={9}
+        level={5}
       >
       </Map>
     </Main>
