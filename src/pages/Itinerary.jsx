f84a4306c901b5435bcb86c295da6f88522faea4
Ji-Sung05 <dlwltjd6847@gmail.com>
2024-10-28 22:25:46 +0900

    Feat: "마커와 선 구현"

--- src/pages/Itinerary.jsx
+++ src/pages/Itinerary.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Main from '../components/layout/Main';
-import { Map } from 'react-kakao-maps-sdk';
+import { Map, MapMarker, Polyline } from 'react-kakao-maps-sdk';
 import { useSelector } from 'react-redux';
+import { calculateMidpoint } from './../utils/calculateMidpoint';
 
 const Itinerary = () => {
   const data = useSelector((state) => state.travel.data);
@@ -9,18 +10,41 @@ const Itinerary = () => {
 
   const day = useSelector((state) => state.day.day);
   const markers = schedule[day-1]?.activities || [];
+
+  const midpoint = calculateMidpoint(markers);
+
   const mapStyles = {
     width: 'calc(100vw - 498px)',
     height: '100vh',
     zIndex: 10
   }
+
+  const linePath = markers.map((marker) => ({
+    lat: marker.location.x,
+    lng: marker.location.y,
+  }));
+  
   return (
     <Main>
       <Map
-        center={{ lat: 33.5563, lng: 126.79581 }}
+        center={{ lat: midpoint.lat, lng: midpoint.lng }}
         style={mapStyles}
-        level={5}
+        level={8}
       >
+        {markers.map((marker, i) => (
+          <MapMarker 
+            key={i} 
+            position={{ lat: marker.location.x, lng: marker.location.y }} 
+            title={marker.name} />
+        ))}
+
+        <Polyline
+          path={linePath.map((point) => ({lat: point.lat, lng: point.lng }))} 
+          strokeWeight={5}
+          strokeColor="#ff0044"
+          strokeOpacity={0.7} 
+          strokeStyle="solid"
+        />
       </Map>
     </Main>
   )
