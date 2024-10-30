19871aeb8210645113bc8a21ca14d99672c864c7
Ji-Sung05 <dlwltjd6847@gmail.com>
2024-10-30 22:37:41 +0900

    Feat: "DateCalendar 주석 생성"

--- src/components/DateCalendar.jsx
+++ src/components/DateCalendar.jsx
@@ -1,15 +1,19 @@
-import 'react-date-range/dist/styles.css';
-import 'react-date-range/dist/theme/default.css';
-import { DateRangePicker } from 'react-date-range';
 import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+// 라이브러리
+import { DateRangePicker } from 'react-date-range';
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
+// 리덕스
 import { setStartDate, setEndDate } from '../app/travleSlice';
+// 컴포넌트
 import Button from './items/Button';
 
 export const DateCalendar = ({ onClick }) => {
   const dispatch = useDispatch();
   const travel = useSelector((state) => state.travel);
 
+  // 유효한 날짜인지 확인하는 함수
   const getValidDate = (date) => {
     const parsedDate = new Date(date);
     return isNaN(parsedDate.getTime()) ? new Date() : parsedDate;
@@ -22,7 +26,7 @@ export const DateCalendar = ({ onClick }) => {
       key: 'selection',
     }
   ]);
-
+  // 달력에서 날짜 선택하면 날짜 변경
   const handleDateChange = (item) => {
     const { startDate, endDate } = item.selection;
     setState([item.selection]);
@@ -30,7 +34,7 @@ export const DateCalendar = ({ onClick }) => {
     dispatch(setStartDate(startDate.toLocaleDateString('sv-SE')));
     dispatch(setEndDate(endDate.toLocaleDateString('sv-SE')));
   };
-
+  // 달력 컴포넌트에서 필요없는 필드 제거
   useEffect(() => {
     const elements = document.getElementsByClassName('rdrDefinedRangesWrapper');
     if (elements.length > 0) {
@@ -48,7 +52,7 @@ export const DateCalendar = ({ onClick }) => {
         ranges={state}
         direction="horizontal"
       />
-      <div>
+      <div style={{borderRadius: '6px'}}>
         <Button onClick={onClick} text={'완료'} />
       </div>
     </div>
