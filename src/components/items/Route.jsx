4a4b103ad5274c2fb48cfc5de5943b930869f7f9
Ji-Sung05 <dlwltjd6847@gmail.com>
2024-11-01 22:38:16 +0900

    Feat: "Route 컴포넌트 대체 이미지 생성"

--- src/components/items/Route.jsx
+++ src/components/items/Route.jsx
@@ -8,14 +8,14 @@ const Route = ({ data, index, onClick }) => {
       <div className='route__inner'>
         <div className='route__top'>
           <div className='route__order'>{index}</div>
-          <div className='route__time'>18:27</div>
+          {/* <div className='route__time'>18:27</div> */}
         </div>
         <div className='route__middle'>
           <div className='route__desc'>
             <div>{data.name}</div>
             <div className='desc'>{data.description}</div>
           </div>
-          <div className='route__img'><img src="/img/성산일출봉.jpg" alt="장소" /></div>
+          <div className='route__img'><img src={data.image ? data.image : "/img/noimg2.png"}  alt="장소" /></div>
         </div>
         {/* <div className='route__bottom'>
           <div style={{paddingTop: '4px', paddingLeft: '1px'}}>
